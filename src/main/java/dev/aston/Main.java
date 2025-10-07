ddd83a0225957b4d125c228dcdc6932cd319b34b
@rushan_zaripov <suck29@yandex.ru>
2025-10-07 21:33:27 +0300

    feat(menu-loop): добавил вызов меню создания обьекта сконтекстным меню для этого функционала

--- src/main/java/dev/aston/Main.java
+++ src/main/java/dev/aston/Main.java
@@ -1,27 +1,12 @@
 package dev.aston;
 
-import dev.aston.fill.*;
-import dev.aston.fill.FileFounder.JsonParse;
+import dev.aston.MenuNavigator.MenuNavigator;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        InitCollection initCollection = new InitCollection();
-        ObjectAddService objectAddService = new ObjectAddService(initCollection);
-
-//        ManuallyFill manuallyFill= new ManuallyFill(objectAddService);
-//        RandomFill randomFill = new RandomFill(objectAddService);
-        JsonParse jsonParse = new JsonParse(objectAddService);
-        FileFill fileFill = new FileFill(jsonParse);
-
-//        manuallyFill.fillWithPhones();
-//        randomFill.fillWithPhones();
-//        randomFill.fillWithCars();
-//        randomFill.fillWithPersons();
-
-        fileFill.fillWithAllObjects();
-
-        initCollection.print(initCollection.getList());
+        MenuNavigator menuNavigator = new MenuNavigator();
+        menuNavigator.runOurProject();
     }
 }
