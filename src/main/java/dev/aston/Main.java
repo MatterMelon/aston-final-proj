70654be9b8b5e8877181b32b9a2ca589b2660ca9
@rushan_zaripov <suck29@yandex.ru>
2025-10-09 19:55:36 +0300

    feat(menu_loop) Меню

--- src/main/java/dev/aston/Main.java
+++ src/main/java/dev/aston/Main.java
@@ -1,7 +1,12 @@
 package dev.aston;
 
+import dev.aston.MenuNavigator.MenuNavigator;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        MenuNavigator menuNavigator = new MenuNavigator();
+        menuNavigator.runOurProject();
+    }
 }
