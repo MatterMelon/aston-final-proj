7b8eb43683f36ddc13791d7ec904f3b7517c029f
Andrey Popov <dil2232@gmail.com>
2025-10-14 13:24:20 +0600

    refactor(main): удалил подсказку

--- src/main/java/dev/aston/Main.java
+++ src/main/java/dev/aston/Main.java
@@ -2,8 +2,6 @@ package dev.aston;
 
 import dev.aston.MenuNavigator.MenuNavigator;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         MenuNavigator menuNavigator = new MenuNavigator();
