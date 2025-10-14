0cc6975587065d8828f680d08790cc1907fd8cab
Andrey Popov <dil2232@gmail.com>
2025-10-14 19:27:30 +0600

    feat(sort): добавил сортировку по всем полям

--- src/main/java/dev/aston/sort/BubbleSort.java
+++ src/main/java/dev/aston/sort/BubbleSort.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class BubbleSort<T> implements SortStrategy<T> {
+public class BubbleSort<T extends Comparable<? super T>> implements SortStrategy<T> {
 
     @Override
     public Collection<T> sort(Collection<? extends T> c, Comparator<? super T> comparator) {
@@ -15,7 +15,7 @@ public class BubbleSort<T> implements SortStrategy<T> {
         for (int i = 0; i < list.size() - 1; i++) {
             swapped = false;
             for (int j = 0; j < list.size() - i - 1; j++) {
-                if (comparator.compare(list.get(j), list.get(j + 1)) > 0) {
+                if (compare(list.get(j), list.get(j + 1), comparator) > 0) {
                     Collections.swap(list, j, j + 1);
                     swapped = true;
                 }
@@ -25,56 +25,6 @@ public class BubbleSort<T> implements SortStrategy<T> {
         return list;
     }
 
-    //    @Override
-//    public Collection<T> parallelSort(Collection<? extends T> c, Comparator<? super T> comparator) throws InterruptedException{
-//        List<T> list = new ArrayList<>(c);
-//        int n = list.size();
-//        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-//        AtomicBoolean swapped = new AtomicBoolean(true);
-//
-//        while (swapped.get()) {
-//            swapped.set(false);
-//
-//            // Чётные индексы
-//            List<Callable<Void>> tasksEven = new ArrayList<>();
-//            for (int i = 0; i <= n - 2; i += 2) {
-//                final int idx = i;
-//                tasksEven.add(() -> {
-//                    synchronized (list) { // синхронизация для безопасного обмена
-//                        if (comparator.compare(list.get(idx), list.get(idx + 1)) > 0) {
-//                            Collections.swap(list, idx, idx + 1);
-//                            swapped.set(true);
-//                        }
-//                    }
-//                    return null;
-//                });
-//            }
-//            System.out.println(tasksEven);
-//
-//
-//            executor.invokeAll(tasksEven);
-//
-//            // Нечётные индексы
-//            List<Callable<Void>> tasksOdd = new ArrayList<>();
-//            for (int i = 1; i <= n - 2; i += 2) {
-//                final int idx = i;
-//                tasksOdd.add(() -> {
-//                    synchronized (list) {
-//                        if (comparator.compare(list.get(idx), list.get(idx + 1)) > 0) {
-//                            Collections.swap(list, idx, idx + 1);
-//                            swapped.set(true);
-//                        }
-//                    }
-//                    return null;
-//                });
-//            }
-//            executor.invokeAll(tasksOdd);
-//        }
-//
-//        executor.shutdown();
-//
-//        return list;
-//    }
     @Override
     public Collection<T> parallelSort(Collection<? extends T> c, Comparator<? super T> comparator) throws InterruptedException {
         List<T> list = new ArrayList<>(c);
@@ -88,7 +38,7 @@ public class BubbleSort<T> implements SortStrategy<T> {
             Callable<Void> evenTask = () -> {
                 for (int i = 0; i <= n - 2; i += 2) {
                     synchronized (list) {
-                        if (comparator.compare(list.get(i), list.get(i + 1)) > 0) {
+                        if (compare(list.get(i), list.get(i + 1), comparator) > 0) {
                             Collections.swap(list, i, i + 1);
                             swapped.set(true);
                         }
@@ -100,7 +50,8 @@ public class BubbleSort<T> implements SortStrategy<T> {
             Callable<Void> oddTask = () -> {
                 for (int i = 1; i <= n - 2; i += 2) {
                     synchronized (list) {
-                        if (comparator.compare(list.get(i), list.get(i + 1)) > 0) {
+                        if (compare(list.get(i), list.get(i + 1), comparator
+                        ) > 0) {
                             Collections.swap(list, i, i + 1);
                             swapped.set(true);
                         }
@@ -117,5 +68,12 @@ public class BubbleSort<T> implements SortStrategy<T> {
         executor.shutdown();
 
         return list;
+    }
+
+    private int compare(T a, T b, Comparator<? super T> comparator) {
+        if (comparator != null) {
+            return comparator.compare(a, b);
+        }
+        return a.compareTo(b);
     }
 }
